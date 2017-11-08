a614b668716d68f32bda6e88d231fc57c7d36a33
Guilherme de Paula <ps.guips@gmail.com>
2017-11-08 21:25:04 -0200

    Teste IF

--- app/shared/user/user.service.ts
+++ app/shared/user/user.service.ts
@@ -1,4 +1,5 @@
 //Servisso para conexão com back-ending.
+//teste
 import { Injectable } from "@angular/core";
 import { Http, Headers, Response } from "@angular/http";
 import { Observable } from "rxjs/Rx";
