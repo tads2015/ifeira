1ded3ba227cc712d533f1e73e5dc0cc461c89469
Guilherme de Paula <ps.guips@gmail.com>
2017-11-14 11:24:06 -0200

    Rota back-end
    Foi alterado a rota para o back-end

--- app/shared/config.ts
+++ app/shared/config.ts
@@ -1,5 +1,5 @@
 //Objeto que contem o link de onde esta sendo hosteado o back-ending.
 export class Config {
-    static apiUrl = "https://ifeira.000webhostapp.com/"
+    static apiUrl = "http://158.69.87.152:8000/"
     static token = "";
 }
