ff5b190df0ce18b9c6e526961ef24d81510892ac
gabrhyiel <33043926+gabrhyiel@users.noreply.github.com>
2017-12-02 13:58:08 -0200

    Hehe

--- app/shared/userS/userS.ts
+++ app/shared/userS/userS.ts
@@ -4,8 +4,6 @@ import { Observable } from "tns-core-modules/data/observable/observable";
 import { ObservableArray } from "tns-core-modules/data/observable-array/observable-array";
 export class UsuarioS{
     static LEntrega:Endereco [];
-    static teste = new ObservableArray(String);
     static array = new ObservableArray(Endereco);
-    test.push("ok");
-};
+}
     
