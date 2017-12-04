f152571e980a2bb31976e15b5aeed0bc1383bf68
idenilson95 <33039464+idenilsoncode@users.noreply.github.com>
2017-12-04 21:08:00 -0200

    App Local
    04/12/2017
    21:07

--- app/pages/cadProdutor/cadProdutos/cadProdutos.component.ts
+++ app/pages/cadProdutor/cadProdutos/cadProdutos.component.ts
@@ -1,13 +1,9 @@
 import { Component } from "@angular/core";
 import { Page } from "ui/page";
-//import {DadosProdutor} from "../../../shared/dadosProdutor/dadosProdutor"
-//import {DadosProdutorService} from "../../../shared/dadosProdutor/dadosProdutor.service";
-
 import { Router } from "@angular/router";
 
 @Component({
     selector: "cadProdutos",
-    //providers: [DadosProdutorService],
     templateUrl: "pages/cadProdutor/cadProdutos/cadProdutos.html",
     styleUrls: ["pages/cadProdutor/cadProdutos/cadProdutos.css"]
 })
